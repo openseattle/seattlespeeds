ea52bfd462a66e7e3501f6b1f820f3a380da69de
Will Scott <willscott@gmail.com>
2015-04-19 14:26:21 -0700

    express

---
+++ index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function(req, res) {
+  res.send('hello world');
+});
+
