b4e1cf173427f428f6d0d87d39fb5e864c1e3a23
Per Nilsson <per.nilsson@formidablelabs.com>
2015-04-23 22:59:22 -0700

    Remove cruft comments

--- setup-db.js
+++ setup-db.js
@@ -7,9 +7,6 @@ var fs = require('fs');
 console.log('Creating empty sqlite database file at ' + config.sqlite.filePath);
 fs.writeFileSync(config.sqlite.filePath, '');
 
-// ip address
-// created_at
-
 dbConnection.run('CREATE TABLE test_results (json_data TEXT, ip_address TEXT, created_at TEXT)');
 
 console.log('sqlite db schema created\n');
